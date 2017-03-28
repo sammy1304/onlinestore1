76b6a277ed848f462c26a3015dae755244646047
sammy <sammtimm7@gmail.com>
2017-03-28 14:09:35 +0100

    changes2

--- www/test_sandbox.php
+++ www/test_sandbox.php
@@ -5,9 +5,14 @@ define('DBUSER', 'root');
 define('DBPASS', 'samuel');
 
 
-$conn = new PDO('mysql:host=localhost; dbname ='.DBNAME, DBUSER, DBPASS);
+try {# prepare a pdo instance
+	$conn = new PDO('mysql:host=localhost; dbname ='.DBNAME, DBUSER, DBPASS);
 
-//echo
+	#set verbose error modes
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
 
 ?>
