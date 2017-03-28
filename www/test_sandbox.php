f28549f5f16e03c81b3dfbd215995d6fbe932cc8
sammy <sammtimm7@gmail.com>
2017-03-28 16:54:53 +0100

    changes5

--- www/test_sandbox.php
+++ www/test_sandbox.php
@@ -15,10 +15,25 @@ try {# prepare a pdo instance
 	echo $e->getMessage();
 }*/
 
+#max file size....
+define("MAX_FILE_SIZE", "2097152");
+
+if (array_key_exists('save', $_POST)) {$errors = [];
+
+	#be sure a file was selected...
+	
+	if(empty($_FILES['pic']['name'])){
+		$errors[] = "please choose a file";
+	}
 
 
-if (array_key_exists('save', $_POST)) {
-	print_r($_FILES);
+  # check file size...
+		if (empty($_FILES['pic']['size'] > MAX_FILE_SIZE) {
+			$errors[] = "file size exceeds maximum. maximum:". MAX_FILE_SIZE;
+		}
+
+		if(empty($errors)) {echo "done";} else {foreach ($errors as $err){echo $err.'</br>';}
+	}
 }
 
 ?>
