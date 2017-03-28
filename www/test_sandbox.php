ec8ae41772f706dca3a43e52d380fb7d5fa0e723
sammy <sammtimm7@gmail.com>
2017-03-28 16:30:49 +0100

    changes4

--- www/test_sandbox.php
+++ www/test_sandbox.php
@@ -1,6 +1,6 @@
 <?php    # test.php sandbox
 
-define('DBNAME', 'onlinestore1');
+/*define('DBNAME', 'onlinestore1');
 define('DBUSER', 'root');
 define('DBPASS', 'samuel');
 
@@ -13,6 +13,19 @@ try {# prepare a pdo instance
 
 }catch(PDOException $e){
 	echo $e->getMessage();
+}*/
+
+
+
+if (array_key_exists('save', $_POST)) {
+	print_r($_FILES);
 }
 
 ?>
+
+ <form id="register" method="POST" enctype="multipart/form-data">
+ <p>please upload a file</p>
+ <input type="file" name="pic">
+
+ <input type="submit" name="save">
+ </form>
