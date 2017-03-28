d3d60f917f1f96939aaf3ca680724043e1a6a4b1
sammy <sammtimm7@gmail.com>
2017-03-28 14:20:23 +0100

    this is test_sandbox changes3

--- www/test_sandbox.php
+++ www/test_sandbox.php
@@ -9,7 +9,7 @@ try {# prepare a pdo instance
 	$conn = new PDO('mysql:host=localhost; dbname ='.DBNAME, DBUSER, DBPASS);
 
 	#set verbose error modes
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
 }catch(PDOException $e){
 	echo $e->getMessage();
